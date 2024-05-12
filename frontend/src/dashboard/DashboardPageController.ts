b1854962f6955aa26d16349194765cea740a5186
Abdulrahman Ibrahim <89452130+Abdulrahman295@users.noreply.github.com>
2024-05-12 17:06:49 +0300

    add: transaction table

--- frontend/src/dashboard/DashboardPageController.ts
+++ frontend/src/dashboard/DashboardPageController.ts
@@ -2,9 +2,17 @@ import User from "../common/Classes/User";
 import PageController from "../common/Interfaces/PageController";
 
 export default class DashboardPageController implements PageController {
+  public run(user: User): void {
+    console.log("Running DashboardPageController");
+  }
 
-    public run(user: User): void {
-        console.log("Running DashboardPageController");
-    }
-    
+  public handleDelete(): void {}
+
+  public handleEdit(): void {}
+
+  public handleExpand(): void {}
+
+  public search(): void {}
+
+  public fillTransactionTable(user: User): void {}
 }
