f35d1d9e851fe71cbcdf94687822e1c76d342b46
Ahmed <ahmedsherif602@gmail.com>
2024-05-16 21:23:02 +0300

    branch was Updated

--- frontend/src/homepage/HomePageController.ts
+++ frontend/src/homepage/HomePageController.ts
@@ -1,3 +1,4 @@
+import Transaction from "../common/Classes/Transaction";
 import User from "../common/Classes/User";
 import PageController from "../common/Interfaces/PageController";
 import Chart from 'chart.js/auto'
@@ -5,18 +6,20 @@ import Chart from 'chart.js/auto'
 export default class HomePageController implements PageController {
     public run(user: User): void {
         console.log(user);
-        const burger = document.getElementById('burger');
-        const closeMenuBtn = document.getElementById("close");
-        const menu = document.getElementById("menu");
-
-        const data = [
+        const transactions = user.getAllTransactions();
+        console.log(transactions);
+        const categories = user.getCategories();
+        // const burger = document.getElementById('burger');
+        // const closeMenuBtn = document.getElementById("close");
+        // const menu = document.getElementById("menu");
+        const categoryData = [
             {category:"Food", amount: 100},
             {category:"Entertainment", amount: 200},
             {category:"Utilities", amount: 100},
             {category:"shopping", amount: 100},
         ]
-        const x = data.map(genre => genre.category); 
-        const y = data.map(genre => genre.amount); 
+        const x = categoryData.map(genre => genre.category); 
+        const y = categoryData.map(genre => genre.amount); 
         const color = ["#1E2760","#1D4ED8","#7A2048","blue"]
         // const color = ["#37A2EB","#FF6383","#FF9F40","#FFCD56"]
         let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
@@ -28,6 +31,35 @@ export default class HomePageController implements PageController {
             if(i % 2 === 0) monthlyExpense.push(20 + 5*i);
             else monthlyExpense.push(40 - 3*i);
             total.push(monthlyIncome[i] - monthlyExpense[i]);
+        }
+        const yearButton = document.getElementById("year");
+        yearButton.onchange = (e:MouseEvent)=>{
+            e.preventDefault();
+            const target = e.target as HTMLInputElement;
+            const year = target.value;
+            console.log(year)
+            const data = transactions.filter(transaction=>transaction.year === year);
+        }
+        window.onload = ()=>{
+            const recent = transactions.splice(transactions.length-10);
+            const recentContainer = document.getElementById("my-transactions");
+            for(let i=0;i<recent.length;i++){
+                const transaction = document.createElement("div");
+                const name = document.createElement("div");
+                const amount = document.createElement("div");
+                name.innerHTML = recent[i].getName();
+                if(recent[i].getType() === "income"){
+                    amount.innerHTML = "+" + recent[i].getAmount().toString();
+                    amount.style.color = "green;"
+                }else{
+                    amount.innerHTML = "-" + recent[i].getAmount().toString();
+                    amount.style.color = "red;"
+                }
+                transaction.appendChild(name)
+                transaction.appendChild(amount)
+                transaction.classList.add("transaction");
+
+            }
         }
 
         const pieChart = new Chart("pie-chart",{
@@ -177,19 +209,21 @@ export default class HomePageController implements PageController {
         });
         console.log(Chart)
 
-        function openMenu(){
-            menu.style.transform = "translateX(0)";
-            console.log("abc")
-        }
-        function closeMenu(){
-            menu.style.transform = "translateX(100%)";
-            console.log("abc")
-        }
+        
+
+        // function openMenu(){
+        //     menu.style.transform = "translateX(0)";
+        //     console.log("abc")
+        // }
+        // function closeMenu(){
+        //     menu.style.transform = "translateX(100%)";
+        //     console.log("abc")
+        // }
 
 
 
-        burger.addEventListener("click",openMenu)
-        closeMenuBtn.addEventListener("click",closeMenu)
+        // burger.addEventListener("click",openMenu)
+        // closeMenuBtn.addEventListener("click",closeMenu)
     }
 }
 
